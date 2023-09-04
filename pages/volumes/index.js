9d4c677e365f75306653505a7c7538773cefe723
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-04 12:31:42 +0200

    solve lotr-app-dynamic-routes challenge

--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -1,9 +1,21 @@
 import { introduction, volumes } from "../../lib/data";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Overview() {
+  const router = useRouter();
+
+  function handleClick() {
+    const randomVolume = volumes[Math.floor(Math.random() * volumes.length)];
+    router.push(`volumes/${randomVolume.slug}`);
+  }
+
   return (
     <div>
+      <Head>
+        <title>Lord of the Rings</title>
+      </Head>
       <h1>Lord of the Rings</h1>
       <p>{introduction}</p>
       <h2>All Volumes</h2>
@@ -16,6 +28,9 @@ export default function Overview() {
           );
         })}
       </ul>
+      <button type="button" onClick={handleClick}>
+        Show Random Volume
+      </button>
     </div>
   );
 }
